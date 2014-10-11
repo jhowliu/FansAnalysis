39752177c841577ec86754cbc8ed496ce722ce6b
jhowliu <jhowliu@gmail.com>
2014-10-11 19:14:10 +0800

    Add CommentCrawler and LikesCrawler into Crawler

--- Crawler.py
+++ Crawler.py
@@ -3,51 +3,100 @@ import facebook
 import re
 import json
 
-def crawing(graph, Fans_id, args):
-    pattent = re.compile(u'[\U00010000-\U0010ffff]')
-    nextPage = ""
-    messages = []
-    posts = graph.get_object(Fans_id, **args)
 
-    nextPage = "nextpage"
-    
-    while (nextPage != ""):
-        for post in posts['data']:
-            comments = []
-            if ('message' in post):
-                myMSG = re.sub(pattent, '', post['message'])
-                if ('comments' in post):
-                    for cmts in post['comments']['data']:
-                        myCMTS = re.sub(pattent, '', cmts['message'])
-                        comments.append({ 'id':cmts['id'] ,'Author': {'id':cmts['from']['id'], 'name':cmts['from']['name'] }, 'msg':myCMTS, 'like_count': cmts['like_count'], 'Time': cmts['created_time']})
-                
-                if ('likes' in post):
-                    messages.append({'id':post['id'], 'Time':post['created_time'], 'msg':myMSG, 'likes':post['likes']['data'], 'total_likes':len(post['likes']['data']), 'comments': comments})
-                else:
-                    messages.append({ 'id':post['id'], 'Time':post['created_time'], 'msg':myMSG, 'likes':[] , 'total_likes':0, 'comments': comments})
+def CommentCrawler(graph, postid):
+    total = 0
+    commentlist = []
+    args = {'fields': 'like_count, from, message, id, created_time, attachment', 'limit':50, 'after':''}
 
-        if ('paging' in posts):
-            nextPage = posts['paging']['next'] 
+    COMMENTS = graph.get_object(postid + '/comments', **args)
+
+    next_id = "nextPage"
+
+    while next_id != "":
+        total += len(COMMENTS['data'])
+        for comment in COMMENTS['data']:
+            commentlist.append({'id':comment['id'], 'like_count':comment['like_count'], 'Author':{'uid':comment['from']['id'], 'uname':comment['from']['name']}, 'time':comment['created_time'], 'msg':comment['message']})
+
+        if 'paging' in COMMENTS and 'next' in COMMENTS['paging']:
+            next_id = COMMENTS['paging']['cursors']['after'].encode('utf8')
         else:
             break
 
-        nextPage_id = re.search('[0-9]+$', nextPage).group(0)
-        print nextPage_id
-        args['until'] = nextPage_id 
-        posts = graph.get_object(Fans_id, **args)
+        args['after'] = next_id
+        COMMENTS = graph.get_object(postid + '/comments', **args)
+
+    print "Comments: " + str(total)
+    return commentlist
+
+def LikeCrawler(graph, postid):
+    total = 0
+    likelist = []
+    args = {'limit':1000, 'after':'' }
+
+    LIKES = graph.get_object(postid + '/likes', **args)
+
+    next_id = "nextPage"
+
+    while next_id != "":
+        total += len(LIKES['data'])
+        for person in LIKES['data']:
+            likelist.append({ 'uid':person['id'], 'uname':person['name']})
+        next_id = ""
+        if 'paging' in LIKES and 'next' in LIKES['paging']:
+            next_id = LIKES['paging']['cursors']['after'].encode('utf8')
+        else:
+            break
+
+        args['after'] = next_id
+        LIKES = graph.get_object(postid + '/likes', **args)
+
+    print "Likes: " + str(total)
+
+    return likelist, total
+
+
+def crawing(graph, Fans_id, args):
+    pattent = re.compile(u'[\U00010000-\U0010ffff]')
+    next_id = ""
+    messages = []
+    posts = graph.get_object(Fans_id + '/posts', **args)
+
+    next_id = "nextpage"
+
+    while (next_id != ""):
+        # iterate posts
+        for post in posts['data']:
+            post_id = post['id']
+            # member of like
+            likelist, likeCount = LikeCrawler(graph, post_id)
+            commentlist = CommentCrawler(graph, post_id)
+            messages.append({'id':post_id, 'like_count': likeCount, 'time':post['created_time'], 'likes':likelist, 'comments':commentlist})
+        next_id = ''
+        if ('paging' in posts):
+            next_id = re.search('[0-9]+$', posts['paging']['next']).group(0)
+        else:
+            break
+
+        print next_id
+        args['until'] = next_id
+        posts = graph.get_object(Fans_id + '/posts', **args)
+
     return messages
 
 if __name__ == "__main__":
     out = open('Fans.txt', 'w')
-    
-    TOKEN = "CAACEdEose0cBAMGSWnJO92WreoNyooZAE5APuc94DJzjMKjtqLE9qqXLLuMuTCx8lLZAlU8dvukZAbqTopvwd6ni2mZBglo0FeIe6oT6cZCZAZAqFnKdlMMvyxlCl68ZAri2ajAhqP9M8pfrcLDJgQYTabIN8rh0YLiyM2k4C9RqWovj9MGaqcZAdteX6Uh6sV07vbIjDFMZAoyDbDcNudZB0tOfPLGkv61F5cZD"
 
-    graph = facebook.GraphAPI(TOKEN)
-      
-    Fans_id = '544241848968882/posts';
-    
-    args = {'fields' : 'id, message, likes, comments', 'limit' : 20, 'until' : ''}
-    
+    Token = 'CAACEdEose0cBANKH2QjO5gwXVYqikntPfPNH4F6iY7ZC8nLsIqsiPItZCytVWWclhOot6y3hXJIdLmOb2oVVawaPe3mZBRxo6kIBLDZCsBTkZBPo1mXzeevb0n4BrpNQdCCHqlG7VNWKF36HSTR8j2LPoCNp2ZCqbZBrSZAl0gBu51pGcjEcs1K3jIgwXNnW92rqRbMQW8RvXnU7a08JaIfJfW49qb49bkYZD'
+
+    graph = facebook.GraphAPI(Token)
+
+    #Fans_id = '544241848968882'
+    Fans_id = '136845026417486'
+    #Post_id = '544241848968882_810446749015056'
+
+    args = {'fields' : 'id, message, likes, comments', 'limit' : 5, 'until' : ''}
+
     messages = crawing(graph, Fans_id, args)
 
     out.write(json.dumps(messages, ensure_ascii=False, indent=2).encode("utf8"))
