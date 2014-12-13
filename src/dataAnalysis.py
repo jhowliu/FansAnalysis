2f6cf088dafe3494f1ebed90da414b10b767acf5
jhowliu <jhowliu@gmail.com>
2014-12-14 01:44:58 +0800

    Add filter postid respectively function

--- src/dataAnalysis.py
+++ src/dataAnalysis.py
@@ -1,5 +1,5 @@
 import sys
-import numpy
+import numpy as np
 
 def loadFileToDict(fileName):
     tmp = open(fileName)
@@ -8,9 +8,9 @@ def loadFileToDict(fileName):
     for line in tmp.readlines():
         data = line.replace('\r\n', '').split(',')
         if data[0] == 'uid':
-            label = data[1:len(data)-1]
+            label = data[1:len(data)]
         else:
-            userDict[data[0]] = map(lambda x: int(x), data[1:len(data)-1])
+            userDict[data[0]] = map(lambda x: int(x), data[1:len(data)])
 
     return userDict, label
 
@@ -20,13 +20,22 @@ def loadClusteringResult(fileName):
 
     for line in tmp.readlines():
         result.append(line.replace('\n', '').split(','))
-    
+
     return result
+
+# Return postid respectively
+def filterPK(label):
+    label = np.array(label)
+
+    pig = filter(lambda x: x.find('652438848137404') == 0, label)
+    kp  = filter(lambda x: x.find('136845026417486') == 0, label)
+
+    return pig, kp
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print "Usage: python dataAnalysis.py <UserMatrix> <ClusteringResult>"
         sys.exit(1)
-    
+
     user, label = loadFileToDict(sys.argv[1])
     result = loadClusteringResult(sys.argv[2])
