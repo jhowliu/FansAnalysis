25b1248b6178d3fac582f6d47af9d3f6c2857ce8
Jhowliu <jhowliu@gmail.com>
2014-12-17 22:03:44 +0800

    Update dataAnalysis

--- src/dataAnalysis.py
+++ src/dataAnalysis.py
@@ -1,5 +1,5 @@
 import sys
-import numpy
+import numpy as np
 
 def loadFileToDict(fileName):
     tmp = open(fileName)
@@ -8,9 +8,9 @@ def loadFileToDict(fileName):
     for line in tmp.readlines():
         data = line.replace('\r\n', '').split(',')
         if data[0] == 'uid':
-            label = data[1:len(data)-1]
+            label = data[1:len(data)]
         else:
-            userDict[data[0]] = map(lambda x: int(x), data[1:len(data)-1])
+            userDict[data[0]] = map(lambda x: int(x), data[1:len(data)])
 
     return userDict, label
 
@@ -20,13 +20,28 @@ def loadClusteringResult(fileName):
 
     for line in tmp.readlines():
         result.append(line.replace('\n', '').split(','))
-    
+
     return result
+
+def getClusterPost(result, user, label, threshold):
+    data = []
+    label = np.array(label)
+    for t in result:
+        tmp = []
+        for uid in t[1:len(t)]:
+            tmp.append(user[uid])
+
+        tmp = np.array(tmp)
+        tmp = label[np.sum(tmp) > threshold]
+
+        data.append(tmp.tolist())
+
+    return data
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print "Usage: python dataAnalysis.py <UserMatrix> <ClusteringResult>"
         sys.exit(1)
-    
+
     user, label = loadFileToDict(sys.argv[1])
     result = loadClusteringResult(sys.argv[2])
